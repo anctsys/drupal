75b3cd7e7267c893dba5c6dbaeaa66cd8dc48e31
nctsysteme <nctsysteme@gmail.com>
2021-02-20 21:40:56 +0100

    Ajout des données à la classes

--- src/Plugin/RulesAction/RulesRequestAction.php
+++ src/Plugin/RulesAction/RulesRequestAction.php
@@ -1,3 +1,12 @@
 <?php
 
 namespace Drupal\requestaction\Plugin\RulesAction;
+use Drupal\rules\Core\RulesActionBase;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+
+class RulesRequesAction extends RulesActionBase implements ContainerFactoryPluginInterface {
+
+
+}
