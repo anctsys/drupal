bfe581061ada9bb82c732c12275fc5aebef65045
nctsysteme <nctsysteme@gmail.com>
2021-02-22 23:33:30 +0100

    Ne marche pas suppression de entity node

--- rules_http_request/src/Plugin/RulesAction/RulesHttpRequest.php
+++ rules_http_request/src/Plugin/RulesAction/RulesHttpRequest.php
@@ -153,14 +153,14 @@ class RulesHttpRequest extends RulesActionBase implements ContainerFactoryPlugin
    */
 
 //protected function doExecute () {
-protected function doExecute(array $url, $linkurl, $nodetype, $apiuser, $apipass, $apitoken, $content_author, $post_title, $post_body ,$node_body) {
+protected function doExecute(array $url, $linkurl, $nodetype, $apiuser, $apipass, $apitoken, $content_author, $post_title, $post_body /*,$node_body*/) {
 // Debug message
 drupal_set_message(t("Activating Rules API POST ..."), 'status');
-
+/*
 $serializer = \Drupal::service('serializer');
 $node = Node::load(2);
 $data = $serializer->serialize($node_body, 'json', ['plugin_id' => 'entity']);
-
+*/
 $serialized_entity = json_encode([
   'title' => [['value' => $post_title]],
   'type' => [['target_id' => $nodetype ]],
@@ -182,7 +182,7 @@ $options = [
   ],
 'timeout' => '2',
 'body' => $serialized_entity,
-'node' => $data
+//'node' => $data
 'headers' => [
 'Content-Type' => 'application/hal+json',
 'Accept' => 'application/hal+json',
